e70f60407154769a0817e21576b6064a18363201
Guilherme <Guilhermer1234@gmail.com>
2022-09-18 16:28:43 -0300

    Req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+# https://stackoverflow.com/questions/5899497/how-can-i-check-the-extension-of-a-file
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return print('Formato inválido', file=sys.stderr)
+    # elif path_file == None:
+    #     return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+    # else:
+    try:
+        with open(path_file, 'r') as arquivo:
+            news = arquivo.read()
+            news_formated = news.split('\n')
+        
+            return news_formated
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
