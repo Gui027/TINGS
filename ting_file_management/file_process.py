d9d10ce6efdf6c3ad81e155dc8b62596a93fc159
Guilherme <Guilhermer1234@gmail.com>
2022-09-19 17:54:19 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+# https://www.geeksforgeeks.org/how-to-print-to-stderr-and-stdout-in-python/
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    
+    for i in instance._data:
+        while i['nome_do_arquivo']:
+            if i['nome_do_arquivo'] == path_file:
+                return None
+
+    print_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+    
+    instance.enqueue(print_file)
+    return  sys.stdout.write(str(print_file))
+  
 
 
 def remove(instance):
