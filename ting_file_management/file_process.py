0d456ebc9b8798af9aa1808f617a06dcfdf34a8b
Guilherme <Guilhermer1234@gmail.com>
2022-09-19 22:35:41 -0300

    req4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -21,7 +21,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) < 1:
+        return print('Não há elementos', file=sys.stdout)
+    else: 
+        path_file = instance.dequeue()['nome_do_arquivo']
+        return print(f'Arquivo {path_file} removido com sucesso', file=sys.stdout)
 
 
 def file_metadata(instance, position):
